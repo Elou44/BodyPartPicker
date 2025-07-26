c66c9867207ada6919f356bd94f4af858397b261
elou44 <elouarn.laine@gmail.com>
2025-07-26 17:07:49 +0200

    ELO - showing bounding box.(9)

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ loader.load(
                 // 4. On déplace directement sa GÉOMÉTRIE.
                 // On la translate de la valeur inverse du centre.
                 // Cela recentre physiquement tous les points du maillage autour de son pivot (0,0,0).
-                child.geometry.translate(-center.x, -center.y + size.y / 2, -center.z);
+                child.geometry.translate(0, 100, 0);
             }
         });
 
